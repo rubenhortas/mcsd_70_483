3cd45e5cfd77e9ab38e20b65a73f64a4efdd1e93
Ruben <ruben.hortas@gmail.com>
2016-07-07 18:31:00 +0200

    Updated

---
+++ Chapter1/Chapter2/ForLoopWithMultipleLoppVariables.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Chapter2
+{
+    public static class ForLoopWithMultipleLoppVariables
+    {
+        public static void Start()
+        {
+            int[] values = { 1, 2, 3, 4, 5, 6 };
+            for(int x=0, y = values.Length -1; ((x < values.Length) && (y >= 0)); x++, y--)
+            {
+                Console.Write(values[x]);
+                Console.Write(values[y]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- Chapter1/Chapter2/Program.cs
+++ Chapter1/Chapter2/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Null_CoalescingOperator.Start();
+            //Null_CoalescingOperator.Start();
+            ForLoopWithMultipleLoppVariables.Start();
         }
     }
 }
